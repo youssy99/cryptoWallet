716bbb0866c0e4d38d2e4a8d158c368fd4001f22
Youssef Yadini <youssefyadini@MacBook-Pro-di-Youssef.local>
2024-03-10 14:01:44 +0100

    Fix some import

--- wallet/wallet/settings.py
+++ wallet/wallet/settings.py
@@ -11,8 +11,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-import django_heroku
-import dj_database_url
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
